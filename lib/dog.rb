f609205b8c931d25c54c9a97b4e275cb059c2fe7
Alla Kuzminova <alchik@gmail.com>
2020-10-17 00:57:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,4 +8,18 @@ class Dog
   def save
     @@all << self
   end
+
+  def self.clear_all
+    @@all.clear
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.print_all
+    @@all.each do |item|
+      puts item
+    end
+  end
 end
